1bfa0055db396e13a3dcdb5fa86b628a098de678
santhoshramu09 <santhouse001@gmail.com>
2022-07-01 19:20:48 +0530

    array program

---
+++ src/org/java/arraydemo.java
@@ -0,0 +1,22 @@
+package org.java;
+
+public class arraydemo {
+	public static void main(String[] args) {
+	int num[]=new int[6];
+	num[0]=10;
+	num[1]=20;
+	num[2]=30;
+	num[3]=40;
+	num[4]=50;
+	num[2]=3000;
+	num[1]=20;
+	num[5]=50;
+	int len=num.length;
+	System.out.println(len);
+	
+	for (int i = 0; i < num.length; i++);
+	System.out.println(num[5]);
+
+	}
+
+}
